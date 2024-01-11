63122559b3bb7c7e82159ae22fc990b715ce2502
bigdata2learn <rakeshgopidi@gmail.com>
2024-01-11 13:43:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
-import pandas
-import requests
+#import pandas
+#import requests
 import snowflake.connector
-from urllib.error import URLError
+#from urllib.error import URLError
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
