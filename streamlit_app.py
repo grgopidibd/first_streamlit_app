cd3b921c51a87c0aa31c524987b626caf31bbf2b
bigdata2learn <rakeshgopidi@gmail.com>
2024-01-11 11:04:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,9 +46,9 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 # fruit header text
-streamlit.text("What fruit would you like to add?")
+#streamlit.text("What fruit would you like to add?")
 # Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('jackfruit')
+#add_my_fruit = streamlit.text_input('jackfruit')
 #add_my_fruit = {'banana','cherry','strawberry','pineapple','apple','mango','coconut','plum','avocado','starfruit'}
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'jackfruit')
 streamlit.write('The user entered', fruit_choice)
